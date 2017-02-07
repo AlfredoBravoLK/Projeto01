f2e466890e32e2770bc47ee229e7337351ae2768
a18984 <a18984@d1-08-08.dped.local>
2017-02-07 15:51:27 +0000

    Versão beta 0.1

--- src/projeto01/Main.java
+++ src/projeto01/Main.java
@@ -1,14 +1,11 @@
 package projeto01;
 
-/**
- *
- * @author João Barbosa
- */
 public class Main {
 
     public static void main(String[] args) {
         // Teste ao GitHub
         System.out.println("Hello GitHub.");
+        System.out.println("How are you ma frend!?");
     }
     
 }
